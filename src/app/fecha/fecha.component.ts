b22be8b6ca52ac19373f3c58a8287c7ec02a307d
carlosjayfr <carlosjfr2@hotmail.com>
2018-03-13 13:59:59 +0100

    Primer commit

---
+++ src/app/fecha/fecha.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app-fecha',
+    templateUrl: './fecha.component.html',
+    styleUrls: ['./fecha.component.css']
+})
+
+export class FechaComponent {
+    hoy:object= new Date(); //Poner ahí object es opcional
+
+}
